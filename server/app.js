6f96e94047ab031539deb4e65c3631117a213834
Aniket Joshi <aniketjosh2606@gmail.com>
2025-10-01 21:18:40 +0530

    feat : ready to deploy

---
+++ server/app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const dotenv = require("dotenv");   // ✅ import dotenv
+
+dotenv.config();  // ✅ load .env variables
+
+
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin");
+const providerRoutes = require("./routes/provider");
+const userRoutes = require("./routes/user");
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// app.use("/resumes", express.static(path.join(__dirname, "resumes")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  next();   
+});
+
+app.use("/auth", authRoutes);
+app.use("/admin", adminRoutes);
+app.use("/provider", providerRoutes);
+app.use("/user", userRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((result) => {
+    console.log("Connected to Database");
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log("This is error");
+    console.log(err);
+  });
